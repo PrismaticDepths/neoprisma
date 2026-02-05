d7cc1b26f668739cdcfd1482fd27df68f0d422f6
prismaticdepths <109403545+PrismaticDepths@users.noreply.github.com>
2026-02-04 19:38:12 -0800

    patches attempt 2

--- src/main.py
+++ src/main.py
@@ -4,10 +4,12 @@ if getattr(sys, "frozen", False):
 	BASE = sys._MEIPASS
 else:
 	BASE = os.path.dirname(__file__)
-
 SRC = os.path.join(BASE, "src")
 if SRC not in sys.path:
 	sys.path.insert(0, SRC)
+
+from upstreampatches import pynput_313
+pynput_313()
 
 if sys.platform == "darwin":
 	import platform_macos
