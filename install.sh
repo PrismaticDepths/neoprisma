7492310107212f588bacad95769d8ce9281c01c7
PrismaticDepths <109403545+PrismaticDepths@users.noreply.github.com>
2026-02-21 21:27:24 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -163,7 +163,7 @@ cd src
 
 echo "Building binaries..."
 
-clang++ -arch $ARCH -O3 -Wall -shared -std=c++17 -undefined dynamic_lookup $(python3 -m pybind11 --includes) playback.cpp -o playback$(python3-config --extension-suffix)
+clang++ -arch $ARCH -O3 -Wall -shared -std=c++17 -undefined dynamic_lookup $(python3 -m pybind11 --includes) playback.cpp -o playback$(python3-config --extension-suffix) automation_macos.cpp
 
 cd ..
 
