f0f764f6130ec9e0cf5cde3654fc1d00cc0f19bb
prismaticdepths <109403545+PrismaticDepths@users.noreply.github.com>
2026-02-25 20:48:00 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -166,7 +166,7 @@ EXT_SUFFIX=$($PYTHON_EXE -c "import sysconfig; print(sysconfig.get_config_var('E
 
 echo "Building binaries..."
 
-clang++ -arch $ARCH -O3 -Wall -shared -std=c++17 -undefined dynamic_lookup $($PYTHON_EXE -m pybind11 --includes) playback.cpp -o playback$($EXT_SUFFIX) automation_macos.cpp
+clang++ -arch $ARCH -O3 -Wall -shared -std=c++17 -undefined dynamic_lookup $($PYTHON_EXE -m pybind11 --includes) playback.cpp -o playback$EXT_SUFFIX automation_macos.cpp
 
 ls
 
