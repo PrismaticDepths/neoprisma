ba26eafd8abc80dfb7561864da39ee4b813820b5
prismaticdepths <109403545+PrismaticDepths@users.noreply.github.com>
2026-02-26 19:59:33 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -75,7 +75,7 @@ if [ "$#" -gt 0 ]; then
 	done
 fi
 
-echo -n "  [..] Checking OS and arch... "
+echo -n "  [..] Checking OS and arch"
 
 die() {
 	echo "$*" >&2
@@ -87,33 +87,33 @@ version_ge() {
 }
 
 # OS check
-[[ "$(uname)" == "Darwin" ]] || die "\r  [\033[31mFAIL\033[0m] Checking OS and arch... \nERROR: invalid OS (must be on macOS)"
+[[ "$(uname)" == "Darwin" ]] || die "\r  [\033[31mFAIL\033[0m] Checking OS and arch\nERROR: invalid OS (must be on macOS)"
 
 # macOS version check
 REQUIRED_MACOS="12.0"
 INSTALLED_MACOS=$(sw_vers -productVersion)
 
 version_ge "$INSTALLED_MACOS" "$REQUIRED_MACOS" \
-	|| die "\r  [\033[31mFAIL\033[0m] Checking OS and arch... \nERROR: macOS $REQUIRED_MACOS+ required (found $INSTALLED_MACOS)"
+	|| die "\r  [\033[31mFAIL\033[0m] Checking OS and arch\nERROR: macOS $REQUIRED_MACOS+ required (found $INSTALLED_MACOS)"
 
 # Architecture check
 ARCH=$(uname -m)
 [[ "$ARCH" == "arm64" || "$ARCH" == "x86_64" ]] \
-	|| die "\r  [\033[31mFAIL\033[0m] Checking OS and arch... \nERROR: unsupported CPU architecture $ARCH (must be arm64 or x86_64)"
+	|| die "\r  [\033[31mFAIL\033[0m] Checking OS and arch\nERROR: unsupported CPU architecture $ARCH (must be arm64 or x86_64)"
 
-echo "\r  [\033[32mOK\033[0m] Checking OS and arch... "
+echo "\r  [\033[32mOK\033[0m] Checking OS and arch"
 
-echo -n "  [..] Checking for dependencies... "
+echo -n "  [..] Checking for dependencies"
 
 require_cmd() {
-	command -v "$1" >/dev/null 2>&1 || die "\r  [\033[31mFAIL\033[0m] Checking for dependencies... \nERROR: missing dependency $1"
+	command -v "$1" >/dev/null 2>&1 || die "\r  [\033[31mFAIL\033[0m] Checking for dependencies\nERROR: missing dependency $1"
 }
 
 require_cmd git
 require_cmd python3
 require_cmd clang++
 
-echo "\r  [\033[32mOK\033[0m] Checking for dependencies... "
+echo "\r  [\033[32mOK\033[0m] Checking for dependencies"
 
 if [ -d "$BUILD_DIR" ]; then
 	if [[ -n "$BUILD_DIR" ]] && [[ "$BUILD_DIR" != "$HOME" ]] && [[ "$BUILD_DIR" != "/" ]]; then
@@ -138,7 +138,7 @@ if [ -d "$INSTALL_DIR/$APP_NAME.app" ]; then
 			printf "Neoprisma is already installed in the target location ($INSTALL_DIR/$APP_NAME.app). If you are updating the app, this is normal. Replace the existing app and proceed with installation? [y/n] " > /dev/tty
 			read -r yn < /dev/tty
 			case $yn in
-				[Yy]* ) echo break;; # Break the loop and continue script
+				[Yy]* ) break;; # Break the loop and continue script
 				[Nn]* ) echo "Stopping installer..."; exit;; # Exit the script
 				* ) echo "Please answer yes or no.";; # Loop back for invalid input
 			esac
@@ -149,12 +149,11 @@ if [ -d "$INSTALL_DIR/$APP_NAME.app" ]; then
 	else
 		die "INSTALL_DIR/APP_NAME.app is empty or home. Installing to those locations is unsafe."
 	fi
-fi
-echo 
+fi 
 
 echo "Fetching source..."
 
-run_step "Cloning repo into build dir..." git clone -b "$BRANCH" https://github.com/PrismaticDepths/neoprisma "$BUILD_DIR"
+run_step "Cloning repo into build dir" git clone -b "$BRANCH" https://github.com/PrismaticDepths/neoprisma "$BUILD_DIR"
 
 cd "$BUILD_DIR"
 
@@ -181,16 +180,16 @@ echo "Installing Python dependencies... "
 python3 -m venv .venv
 source .venv/bin/activate
 PIP="python3 -m pip"
-run_step "↳ pip... " $PIP install --upgrade pip
-run_step "↳ requirements.txt... " $PIP install -r requirements.txt
-run_step "↳ pyinstaller... " $PIP install pyinstaller 
+run_step "↳ pip" $PIP install --upgrade pip
+run_step "↳ requirements.txt" $PIP install -r requirements.txt
+run_step "↳ pyinstaller" $PIP install pyinstaller 
 
 cd src
 
 PYTHON_EXE=$(which python3 || which python)
 EXT_SUFFIX=$($PYTHON_EXE -c "import sysconfig; print(sysconfig.get_config_var('EXT_SUFFIX'))")
 
-echo "
+echo "Building from source... "
 
 run_step "Building binaries... " clang++ -arch $ARCH -O3 -Wall -shared -std=c++17 -undefined dynamic_lookup $($PYTHON_EXE -m pybind11 --includes) playback.cpp -o playback$EXT_SUFFIX automation_macos.cpp
 
@@ -250,7 +249,7 @@ cat > "$ENTITLEMENTS" <<'EOF'
 </plist>
 EOF
 
-run_step "Signing app..." codesign --force --deep --sign - --options runtime  --entitlements "$ENTITLEMENTS" "$INSTALL_DIR/$APP_NAME.app" 
+run_step "Signing app" codesign --force --deep --sign - --options runtime  --entitlements "$ENTITLEMENTS" "$INSTALL_DIR/$APP_NAME.app" 
 
 echo -n "Cleaning up... "
 if [ -d "$BUILD_DIR" ]; then
