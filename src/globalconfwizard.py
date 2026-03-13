758ddf0178e8f6461c55f2a75622e4c4326feab2
prismaticdepths <109403545+PrismaticDepths@users.noreply.github.com>
2026-03-12 19:06:35 -0700

    Hide dock icon / Settings improvements

--- src/globalconfwizard.py
+++ src/globalconfwizard.py
@@ -26,25 +26,114 @@ SRC = os.path.join(BASE, "src")
 if SRC not in sys.path:
 	sys.path.insert(0, SRC)
 		
+class CNVType:
+
+	@classmethod
+	def from_packed(cls,packed):
+		return cls(cls._unpack(cls,packed))
+
+	def __init__(self,name,real_value):
+
+		self.name = name
+		self.real_value = real_value
+
+	def pack(self):
+		return self._pack()
+	
+	def set_value_from_packed(self,packed):
+		self.real_value = self._unpack(packed)
+	def set_value(self,val):
+		print(val)
+		self.real_value = val
+	
+	def get_value(self):
+		if hasattr(self,"_get_value"):
+			return self._get_value()
+		else:
+			return self.real_value
+
+
+class CNVBoolean(CNVType):
+
+	def __init__(self,value):
+		super().__init__("bool",value)
+
+	def _pack(self):
+		assert type(self.real_value)==type(True)
+		return "yes" if self.real_value==True else "no"
+	def _unpack(self,value:str):
+		if str(value).strip().lower() == "yes":
+			return True
+		elif str(value).strip().lower() == "no":
+			return False
+		else: raise TypeError("cannot unpack non yes/no value")
+	
+class CNVString(CNVType):
+
+	def __init__(self,value):
+		super().__init__("string",value)
+
+	def _pack(self):
+		return str(self.real_value)
+	def _unpack(self,value):
+		return str(value).strip()
+	
+class CNVInteger(CNVType):
+
+	def __init__(self,value,smin=None,smax=None):
+		super().__init__("int",value)
+
+	def _pack(self):
+		return str(self.real_value)
+	def _unpack(self,value):
+		return int(str(value).strip())
+	
+class CNVKeyset(CNVType):
+
+	def __init__(self,value):
+		super().__init__("keyset",value)
+
+	def _pack(self):
+		return " ".join([str(v) for v in self.real_value])
+	
+	def _unpack(self,value):
+		return set(int(v) for v in value.split(" "))
+
+NAME_TO_TYPE = {
+	"bool":CNVBoolean,
+	"string":CNVString,
+	"keyset":CNVKeyset,
+	"int":CNVInteger,
+}
+
 def unpack(fpath):
 		
 	with open(fpath,"r") as cfile:
 		data = {}
 			
 		for line in cfile.readlines():
-			key=line.split("%")[0].strip().upper()
-			val=line.split("%")[1].strip()
-			data[key] = val
-	
-	if not "DOC" in data: raise RuntimeError("Could not unpack configuration file: Missing DOC tag.")
+			tmp = line.split("&",2)
+			typ = tmp[0].strip().lower()
+			try:
+				key = tmp[1].strip().upper()
+			except IndexError:
+				raise RuntimeError("NO_VERSION / Could not unpack configuration file: Possibly corrupted or invalid file, presumably an old version.")
+			val=tmp[2]
+			data[key] = NAME_TO_TYPE[typ].from_packed(val)
+			
+
+	if not "DOC" in data: raise Exception("Could not unpack configuration file: Missing DOC tag.")
 	else:
-		if data["DOC"] != "NEOPRISMA CONFIGURATION DATA": raise RuntimeError("Non-compatible DOC tag.")
+		if data["DOC"].get_value() != "NEOPRISMA CONFIGURATION DATA": raise Exception("Non-compatible DOC tag.")
+	if not "VERSION" in data: raise RuntimeError("NO_VERSION / Could not unpack configuration file: Missing VERSION tag.")
+	else:
+		if data["VERSION"].get_value() < 2: raise RuntimeError("LOW_VERSION / Config version too low")
 	return data
 
-def pack(fpath,data:dict):
+def pack(fpath,data:dict[str,CNVType]):
 
 	with open(fpath,"w") as cfile:
 		packed=""
 		for key,value in data.items():
-			packed += f"{key.upper()}% {value}\n"
+			packed +=f"{value.name}&{key.upper()}&{value.pack()}\n"
 		cfile.write(packed)
--- src/platform_macos.py
+++ src/platform_macos.py
@@ -40,7 +40,7 @@ import time
 import sys
 from threading import Thread
 from PyQt6.QtGui import QAction,QIcon
-from PyQt6.QtCore import QObject,pyqtSignal, QTimer, Qt
+from PyQt6.QtCore import QObject,pyqtSignal, QTimer, Qt, QEvent
 from PyQt6.QtWidgets import (
 	QApplication,
 	QSystemTrayIcon,
@@ -66,9 +66,11 @@ try:
 	import playback
 	import recorder
 	import globalconfwizard
+	from globalconfwizard import CNVKeyset,CNVString,CNVType,CNVBoolean,CNVInteger
 except Exception:
 	tmp=QApplication(sys.argv)
-	QMessageBox.critical(None,"Failed to start Neoprisma!","Fatal error while importing a project component (please report this to the developers!): "+traceback.format_exc(300), QMessageBox.StandardButton.Abort)
+	print(traceback.format_exc(300))
+	QMessageBox.critical(None,"Failed to start Neoprisma!","Please report this issue to the developers! Fatal error while importing a project component: "+traceback.format_exc(300), QMessageBox.StandardButton.Abort)
 	sys.exit(70)
 
 try:
@@ -87,12 +89,15 @@ MACOS_VK_MAP = {
 	24: '=', 27: '-', 33: '[', 30: ']', 42: '\\', 41: ';', 39: "'", 43: ',', 47: '.', 44: '/', 50: '`'
 }
 
-CONFIGURATION_DEFAULTS = {
-	"DOC":"NEOPRISMA CONFIGURATION DATA",
-	"KEYBIND_TOGGLE_RECORD":"59 98",
-	"KEYBIND_TOGGLE_AUTOCLICK":"59 100",
-	"KEYBIND_TOGGLE_PLAYBACK":"59 101",
-	"RELEASE_CHANNEL":"stable"
+CN_CONFIGURATION_DEFAULTS = {
+	"DOC":CNVString("NEOPRISMA CONFIGURATION DATA"),
+	"VERSION":CNVInteger(2),
+	"KEYBIND_TOGGLE_RECORD":CNVKeyset(set([59,98])),
+	"KEYBIND_TOGGLE_AUTOCLICK":CNVKeyset(set([59,100])),
+	"KEYBIND_TOGGLE_PLAYBACK":CNVKeyset(set([59,101])),
+	"RELEASE_CHANNEL":CNVString("stable"),
+	"ABORT_PLAYBACK_ON_INPUT":CNVBoolean(False),
+	"HIDE_APP_ICON":CNVBoolean(True),
 }
 
 MAX_HOTKEY_LEN = 5
@@ -142,14 +147,13 @@ class Main(QObject):
 
 	def __init__(self):
 		super().__init__()
-
 		
 
 		self.app = QApplication(sys.argv)
 
 		try: # force the "about" pane to appear on the left of the system menu bar
 			import AppKit
-			AppKit.NSApp.setActivationPolicy_(AppKit.NSApplicationActivationPolicyRegular)
+			AppKit.NSApp.setActivationPolicy_(AppKit.NSApplicationActivationPolicyAccessory)
 			self.app.setApplicationName("neoprisma")
 			self.menu_bar = QMenuBar(None) 
 			self.about_action = QAction("About Neoprisma", None)
@@ -179,18 +183,44 @@ class Main(QObject):
 			"KEYBIND_TOGGLE_PLAYBACK": set(),
 			"KEYBIND_TOGGLE_AUTOCLICK": set()
 		}
-		self.conf_data=copy.deepcopy(CONFIGURATION_DEFAULTS)
+		self.conf_data=copy.deepcopy(CN_CONFIGURATION_DEFAULTS)
 		if os.path.exists(os.path.expanduser("~/.neoprisma")):
-			conf_data=globalconfwizard.unpack(os.path.expanduser("~/.neoprisma"))
-			for key,value in conf_data.items():
-				self.conf_data[key] = value
+			try:
+				conf_data=globalconfwizard.unpack(os.path.expanduser("~/.neoprisma"))
+			except RuntimeError as e:
+				if str(e).strip().startswith("NO_VERSION"):
+
+					conf_data=copy.deepcopy(CN_CONFIGURATION_DEFAULTS) # Since we don't have the real unpacked version, grab the default values
+
+					with open(os.path.expanduser("~/.neoprisma"),"r") as cfile: # Legacy unpack function
+						for line in cfile.readlines():
+							key=line.split("%")[0].strip().upper()
+							val=line.split("%")[1].strip()
+							conf_data[key].set_value_from_packed(val)
+
+					for key,value in conf_data.items(): # Inefficiently copy the values with the same mechanism normally used
+						self.conf_data[key]=value
+					globalconfwizard.pack(os.path.expanduser("~/.neoprisma"),self.conf_data) # Write the migrated configuration file
+
+				elif str(e).strip().startswith("LOW_VERSION"):
+					pass # migrate to updated version
+
+				else:
+					QMessageBox.warning(None,"Error","Your configuration files appear to be corrupted; please delete hidden file '~/.neoprisma' to reset configurations. Neoprisma will now crash.")
+					raise
+			except Exception as e:
+				QMessageBox.warning(None,"Error","Your configuration files appear to be corrupted; please delete hidden file '~/.neoprisma' to reset configurations. Neoprisma will now crash.")
+				raise
+			else:
+				for key,value in conf_data.items():
+					self.conf_data[key]=value
 		else:
-			self.conf_data=CONFIGURATION_DEFAULTS
+			self.conf_data=copy.deepcopy(CN_CONFIGURATION_DEFAULTS)
 			globalconfwizard.pack(os.path.expanduser("~/.neoprisma"),self.conf_data)
 
 		for key in self.conf_data.keys():
 			if key.startswith("KEYBIND"):
-				self.hotkeys[key] = set(int(i) for i in self.conf_data[key].split(" "))
+				self.hotkeys[key] = self.conf_data[key].get_value()
 
 		self.rebuild_hotkey_lookup()
 
@@ -240,34 +270,25 @@ class Main(QObject):
 		self.settingsw_label = QLabel("Hotkeys are disabled while this window is active.",self.settingsw)
 		self.settingsw_layout.addWidget(self.settingsw_label)
 		self.settingsw_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-		#self.settingsw_speedslider = QSlider()
-		#self.settingsw_speedslider.setRange(0,2)
-		#self.settingsw_speedslider.setValue(1)
-		#self.settingsw_speedslider.valueChanged.connect()
-
 		self.settingsw_hk_layout = QVBoxLayout()
-
 		self.settingsw_hk_rec_layout = QHBoxLayout()
 		self.settingsw_hk_rec = QPushButton("Edit RECORD hotkey",self.settingsw)
 		self.settingsw_hk_rec_disp = QLabel(" + ".join([self.vk_to_name(i) for i in self.hotkeys["KEYBIND_TOGGLE_RECORD"]]))
 		self.settingsw_hk_rec_disp.setAlignment(Qt.AlignmentFlag.AlignRight)
 		self.settingsw_hk_rec_layout.addWidget(self.settingsw_hk_rec)
 		self.settingsw_hk_rec_layout.addWidget(self.settingsw_hk_rec_disp)
-
 		self.settingsw_hk_play_layout = QHBoxLayout()
 		self.settingsw_hk_play = QPushButton("Edit PLAYBACK hotkey",self.settingsw)
 		self.settingsw_hk_play_disp = QLabel(" + ".join([self.vk_to_name(i) for i in self.hotkeys["KEYBIND_TOGGLE_PLAYBACK"]]))
 		self.settingsw_hk_play_disp.setAlignment(Qt.AlignmentFlag.AlignRight)
 		self.settingsw_hk_play_layout.addWidget(self.settingsw_hk_play)
 		self.settingsw_hk_play_layout.addWidget(self.settingsw_hk_play_disp)
-
 		self.settingsw_hk_auto_layout = QHBoxLayout()
 		self.settingsw_hk_auto = QPushButton("Edit AUTOCLICK hotkey",self.settingsw)
 		self.settingsw_hk_auto_disp = QLabel(" + ".join([self.vk_to_name(i) for i in self.hotkeys["KEYBIND_TOGGLE_AUTOCLICK"]]))
 		self.settingsw_hk_auto_disp.setAlignment(Qt.AlignmentFlag.AlignRight)
 		self.settingsw_hk_auto_layout.addWidget(self.settingsw_hk_auto)
 		self.settingsw_hk_auto_layout.addWidget(self.settingsw_hk_auto_disp)
-
 		self.settingsw_hk_rec.clicked.connect(lambda: self.set_hk("KEYBIND_TOGGLE_RECORD"))
 		self.settingsw_hk_play.clicked.connect(lambda: self.set_hk("KEYBIND_TOGGLE_PLAYBACK"))
 		self.settingsw_hk_auto.clicked.connect(lambda: self.set_hk("KEYBIND_TOGGLE_AUTOCLICK"))
@@ -277,7 +298,6 @@ class Main(QObject):
 		self.settingsw_hk_layout.setSpacing(1)
 		self.settingsw_hk_layout.setContentsMargins(0, 0, 0, 0)
 		self.settingsw_layout.addLayout(self.settingsw_hk_layout)
-
 		self.settingsw_speededit = QWidget()
 		self.settingsw_speededit_layout = QHBoxLayout()
 		self.settingsw_speededit.setLayout(self.settingsw_speededit_layout)
@@ -288,8 +308,6 @@ class Main(QObject):
 		self.settingsw_speededit_label = QLabel("(Playback) Speed multiplier:",self.settingsw_speededit)
 		self.settingsw_speededit_layout.addWidget(self.settingsw_speededit_label)
 		self.settingsw_speededit_layout.addWidget(self.settingsw_speededit_input)
-
-
 		self.settingsw_cpsedit = QWidget()
 		self.settingsw_cpsedit_layout = QHBoxLayout()
 		self.settingsw_cpsedit.setLayout(self.settingsw_cpsedit_layout)
@@ -300,10 +318,33 @@ class Main(QObject):
 		self.settingsw_cpsedit_label = QLabel("(Autoclick) Target clicks/second:",self.settingsw_cpsedit)
 		self.settingsw_cpsedit_layout.addWidget(self.settingsw_cpsedit_label)
 		self.settingsw_cpsedit_layout.addWidget(self.settingsw_cpsedit_input)
-
 		self.settingsw_layout.addWidget(self.settingsw_cpsedit)
-
 		self.settingsw_layout.addWidget(self.settingsw_speededit)
+
+		self.settingsw_configbools_layout = QVBoxLayout()
+
+		def add_conf_bool(key,value):
+				tmp=QWidget()
+				tmplayout=QHBoxLayout()
+				tmp.setLayout(tmplayout)
+				nice_label = key.strip().replace("_"," ").title()
+				tmp1=QLabel(nice_label,tmp)
+				tmp2=QCheckBox()
+				tmp2.setChecked(value.real_value)
+				tmp2.checkStateChanged.connect(lambda t: self.conf_data[key].set_value(False if t==Qt.CheckState.Unchecked else True))
+				tmplayout.addWidget(tmp1)
+				tmplayout.addWidget(tmp2)
+				tmplayout.setContentsMargins(0, 0, 0, 0)
+				self.settingsw_configbools_layout.addWidget(tmp)
+
+		for key,value in self.conf_data.items():
+			if value.name == "bool":
+				add_conf_bool(key,value)
+
+		self.settingsw_configbools_layout.setContentsMargins(0, 0, 0, 0)
+		self.settingsw_configbools_layout.setSpacing(1)
+		self.settingsw_layout.addLayout(self.settingsw_configbools_layout)
+
 
 		self.settingsw_layout.setSpacing(5)
 		self.settingsw_hk_layout.setContentsMargins(0, 0, 0, 0)
@@ -312,6 +353,8 @@ class Main(QObject):
 		self.settingsw_save.clicked.connect(self.save_configurations)
 		self.settingsw_layout.addWidget(self.settingsw_save)
 
+		self.settingsw.closeEvent = self.anyw_close
+
 		self.tray.setContextMenu(self.menu)
 
 		self.run_workers = True
@@ -319,6 +362,8 @@ class Main(QObject):
 
 		QTimer.singleShot(0,self.start_hotkeys)
 		QTimer.singleShot(0,self.init_recorder_and_simulator)
+
+		if not self.conf_data["HIDE_APP_ICON"].real_value: self.anyw_open()
 		
 		if self.update_available:
 			QTimer.singleShot(0,self.prompt_update)
@@ -360,8 +405,10 @@ class Main(QObject):
 		winl.addLayout(footl)
 
 		def close_window():
+			win.close()
 			win.destroy()
 		def update():
+			win.close()
 			win.destroy()
 			run_updater()
 			self.shutdown()
@@ -369,14 +416,30 @@ class Main(QObject):
 		dismiss_button.clicked.connect(close_window)
 		update_button.clicked.connect(update)
 
+		win.closeEvent = self.anyw_close
+
+		self.anyw_open()
 		win.show()
 		win.activateWindow()
 		win.raise_()
 
 	def settingsw_popup(self):
+		self.anyw_open()
 		self.settingsw.show()
 		self.settingsw.activateWindow()
 		self.settingsw.raise_()
+
+	def anyw_open(self):
+		import AppKit
+		AppKit.NSApp.setActivationPolicy_(AppKit.NSApplicationActivationPolicyRegular)
+
+	def anyw_close(self, event:QEvent):
+		if not self.conf_data["HIDE_APP_ICON"].real_value:
+			event.accept()
+			return
+		import AppKit
+		AppKit.NSApp.setActivationPolicy_(AppKit.NSApplicationActivationPolicyAccessory)
+		event.accept()
 
 	def upd_speed(self,x):
 		if x == 0: return
@@ -416,7 +479,7 @@ class Main(QObject):
 				self.rebuild_hotkey_lookup()
 				if hk == "KEYBIND_TOGGLE_RECORD": self.recorder.update_hk(self.hotkeys[hk])
 				if hk.startswith("KEYBIND"): 
-					self.conf_data[hk] = " ".join([str(i) for i in self.hotkey_record_buffer])
+					self.conf_data[hk].set_value(self.hotkeys[hk])
 
 	def vk_to_name(self,vk):
 		if vk == 49: return MACOS_VK_MAP[vk]
@@ -435,6 +498,10 @@ class Main(QObject):
 
 			vk = key.vk if isinstance(key,pynput.keyboard.KeyCode) else key.value.vk
 			self.keysdown.add(vk)
+
+			if self.state_playback and self.conf_data["ABORT_PLAYBACK_ON_INPUT"].real_value: 
+				self.toggle_playback()
+				return
 
 			if self.recording_hotkey and len(self.hotkey_record_buffer) < MAX_HOTKEY_LEN:
 				self.hotkey_record_buffer.add(vk)
@@ -595,6 +662,7 @@ try:
 	m = Main()
 except Exception:
 	if QApplication.instance() is None: tmp=QApplication(sys.argv)
-	QMessageBox.critical(None,"Failed to start Neoprisma!","Uncaught exception in class initiation: "+traceback.format_exc(300), QMessageBox.StandardButton.Abort)
+	print(traceback.format_exc(300))
+	QMessageBox.critical(None,"Failed to start Neoprisma!","Please report this issue to the developers! Uncaught exception in class initialization: "+traceback.format_exc(300), QMessageBox.StandardButton.Abort)
 	sys.exit(70)
 sys.exit(m.app.exec())
